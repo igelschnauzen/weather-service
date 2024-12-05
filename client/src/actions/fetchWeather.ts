4b66cce1c7c0dc989f291758fab6021b05928ea3
igelschnauzen <kriegg878@gmail.com>
2024-12-06 00:00:00 +0200

    data view

--- client/src/actions/fetchWeather.ts
+++ client/src/actions/fetchWeather.ts
@@ -2,15 +2,23 @@ interface WeatherData {
     name: string;
     current_temp: number;
     current_feels_like: number;
+    current_humidity: number;
     lat: string;
     lon: string;
     createdAt: string;
     updatedAt: string;
 }
 
-//server action
-export async function fetchWeather(city: string):Promise<any> {
+export async function fetchWeather(city: string):Promise<WeatherData> {
     const response = await fetch(`http://localhost:3000/get/${city}`);
 
-    return response;
+    console.log("this is a server-side action!");
+
+    if(!response.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    const data: WeatherData = await response.json();
+    return data;
+
 }
--- client/src/components/weather.tsx
+++ client/src/components/weather.tsx
@@ -9,22 +9,38 @@ import React from 'react';
 
 export default function Weather() {
   const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("");
+  const [feelsLike, setFeelsLike] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [lastUpdated, setLastUpdated] = useState("");
+
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
   }
 
+  const handleSearch = (city: string): any => {
+    fetchWeather(city).then((data) => {
+      setTemp(data.current_temp.toString());
+      setFeelsLike(data.current_feels_like.toString());
+      setHumidity(data.current_humidity.toString());
+      setLastUpdated(data.updatedAt);
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
     return (
       <div className={`${space_grotesk.className} antialiased bg-white rounded-xl shadow-lg p-6 w-11/12 max-w-md text-center`}>
         <input type='text' placeholder='City name' value={city} onChange={handleChange} className='border-2 rounded'></input>
-        <button className='border-2 rounded m-1' onClick={() => fetchWeather(city)}> <Image className="inline-block" src={loupe} width={20} height={20} alt='Find'/> </button>
+        <button className='border-2 rounded m-1' onClick={() => handleSearch(city)}> <Image className="inline-block" src={loupe} width={20} height={20} alt='Find'/> </button>
 
         <div className="h-px bg-gray-200 my-4"></div>
 
-        <p className="text-lg text-gray-600">Temperature: 25°C</p>
-        <p className="text-lg text-gray-600">Feels Like: 27°C</p>
-        <p className="text-lg text-gray-600">Humidity: 60%</p>
-        <p className="text-sm text-gray-500">Last Updated: 2024-12-04 12:00</p>
+        <p className="text-lg text-gray-600">Temperature: {temp}</p>
+        <p className="text-lg text-gray-600">Feels Like: {feelsLike}</p>
+        <p className="text-lg text-gray-600">Humidity: {humidity}</p>
+        <p className="text-sm text-gray-500">Last Updated: {lastUpdated}</p>
       </div>
     );
 }
